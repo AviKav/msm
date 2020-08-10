6837700e309377a43b40757174e60f9bd11c1c73
AviKav <avikav@avikav.net>
2020-08-10 00:16:36 -0400

    Fix SAVE_ALL pattern match

---
+++ versioning/minecraft/1.16.0.sh
@@ -0,0 +1,7 @@
+# MSM version file for Minecraft 1.16.0 and above
+# Individual changes almost certainly happened earlier.  Testing should be done to find out when
+
+extends "minecraft/1.7.0"
+
+console_command SAVE_ALL:10 "save-all" \
+  "Saved the game"
